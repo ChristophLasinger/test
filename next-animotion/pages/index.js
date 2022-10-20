f700b93037f1788ea7cc07fbb1718a833d42c3c2
ChristophLasinger <lasingerch@gmail.com>
2022-10-20 22:07:58 +0200

    update next

--- next-animotion/pages/index.js
+++ next-animotion/pages/index.js
@@ -1,29 +1,37 @@
 function SelectModel(model) {
   window.localStorage.setItem("vrm", model);
-  window.location.href="webapp/index.html";
+  GoTo("webapp/index.html");
+}
+
+function GoTo(link) {
+  window.location.href = link;
 }
 
 export default function Home() {
   return (
     <>
-    <p id = "logo">Animotion</p>
+    <p id = "main-headline">Animotion</p>
     <div className = "vrm-container">
       <button className="vrm-selector glow-on-hover" type="button" onClick={() => {SelectModel("https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/uwugirl.vrm?v=1652337472157")}}>
-        <img className="vrm-image" src="/images/test.png" alt="virtual model black haired boy with glasses"/>
+        <img className="vrm-image" src="/images/vrms/test.png" alt="virtual model black haired boy with glasses"/>
       </button>
 
       <button className="vrm-selector glow-on-hover" type="button" onClick={() => {SelectModel("https://cdn.glitch.me/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/rich_girl.vrm?v=1663588646001")}}>
-        <img className="vrm-image" src="images/test.png" alt="virtual model black haired boy with glasses"/>
+        <img className="vrm-image" src="/images/vrms/test.png" alt="virtual model black haired boy with glasses"/>
       </button>
   
       <button className="vrm-selector glow-on-hover" type="button" onClick={() => {SelectModel("Model3")}}>
-        <img className="vrm-image" src="images/test.png" alt="virtual model black haired boy with glasses"/>
+        <img className="vrm-image" src="/images/vrms/test.png" alt="virtual model black haired boy with glasses"/>
       </button>
     </div>
 
-    <div className="container">
-      <a href="webapp" className="btn btn-primary">Mediaplayer</a>
-      <a href="about" className="btn btn-primary">About us</a>
+    <div className="link-container">
+      <button className="link-button" role="button" id="player-button" onClick={() => {GoTo("player")}}>
+        Mediaplayer
+      </button>
+      <button className="link-button" role="button" id="about-button" onClick={() => {GoTo("about")}}>
+        Among us
+      </button>
     </div>
     </>
   )
